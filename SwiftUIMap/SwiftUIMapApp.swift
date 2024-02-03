39b1f6d4ddb6f0c5d59c49419487ec11714db68e
Ajay <ajay.nellori@gmail.com>
2024-02-03 11:01:44 +0530

    Initial Commit

---
+++ SwiftUIMap/SwiftUIMapApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIMapApp.swift
+//  SwiftUIMap
+//
+//  Created by Venkata Ajay Sai Nellori on 03/02/24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIMapApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
